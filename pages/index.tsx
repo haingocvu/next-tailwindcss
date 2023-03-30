70857c73c6a21769ed3e56cc0c3264b360c79a0b
Hai Vu Ngoc <ngochai.200290@gmail.com>
2023-03-30 14:54:58 +0700

    add switch component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
+import TwSwitch from '@app/shared/components/form/switch/Switch';
 
 export default function Home() {
   return (
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <TwSwitch defaultEnable={true} />
       </main>
     </>
   );
---
+++ shared/components/form/switch/Switch.tsx
@@ -0,0 +1,59 @@
+import {
+  ComponentPropsWithRef,
+  Dispatch,
+  FC,
+  forwardRef,
+  Fragment,
+  SetStateAction,
+} from 'react';
+import { Switch } from '@headlessui/react';
+
+interface IProps extends ComponentPropsWithRef<any> {
+  name?: string;
+  fallbackLabel?: string;
+  enabled?: boolean;
+  defaultEnable?: boolean;
+  onChange?: Dispatch<SetStateAction<boolean>> | (() => any);
+}
+
+const TwSwitch: FC<IProps> = forwardRef<HTMLInputElement, IProps>(
+  (props, ref) => {
+    const { name, enabled, defaultEnable, fallbackLabel, onChange } = props;
+    return (
+      <Switch.Group>
+        <div className="items-center inline-flex">
+          <Switch.Label className="mr-4 sr-only">{fallbackLabel}</Switch.Label>
+          <Switch
+            name={name}
+            checked={enabled}
+            onChange={onChange}
+            defaultChecked={defaultEnable}
+            as={Fragment}
+          >
+            {({ checked }) => (
+              <button
+                className={`${
+                  checked ? 'bg-blue-600' : 'bg-gray-200'
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span
+                  className={`${
+                    checked ? 'translate-x-6' : 'translate-x-1'
+                  } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                />
+                <input
+                  type="hidden"
+                  name={name}
+                  value={checked.toString()}
+                  ref={ref}
+                />
+              </button>
+            )}
+          </Switch>
+        </div>
+      </Switch.Group>
+    );
+  }
+);
+
+export default TwSwitch;
